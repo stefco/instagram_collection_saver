476c5e658be92954136fc1edb827e82bf8f53e88
Stefan Countryman <stefan.countryman@gmail.com>
2018-05-27 22:24:04 -0500

    add saveImages.php

---
+++ saveImages.php
@@ -0,0 +1,13 @@
+<?php
+
+set_time_limit(0);
+date_default_timezone_set('UTC');
+
+require __DIR__.'/vendor/autoload.php';
+# define $username and $password in auth.php
+require __DIR__.'config.php';
+
+/////// CONFIG ///////
+$debug = true;
+$truncatedDebug = false;
+//////////////////////
