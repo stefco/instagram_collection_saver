367b87e5d920713d068c8185266476aba9893f66
Stefan Countryman <stefan.countryman@gmail.com>
2018-05-27 22:33:55 -0500

    Instagram login working.

--- saveImages.php
+++ saveImages.php
@@ -5,9 +5,20 @@ date_default_timezone_set('UTC');
 
 require __DIR__.'/vendor/autoload.php';
 # define $username and $password in auth.php
-require __DIR__.'config.php';
+require __DIR__.'/auth.php';
 
 /////// CONFIG ///////
-$debug = true;
-$truncatedDebug = false;
+$debug = json_decode(argv[1]);
+$truncatedDebug = json_decode(argv[2]);
 //////////////////////
+
+$ig = new \InstagramAPI\Instagram($debug, $truncatedDebug);
+
+print("Logging in as $username...");
+
+try {
+    $ig->login($username, $password);
+} catch (\Exception $e) {
+    echo 'Something went wrong: '.$e->getMessage()."\n";
+    exit(0);
+}
