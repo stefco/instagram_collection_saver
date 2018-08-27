31ed5b8b9a4f8d9089a57dc6a8a75dfdd7167938
Stefan Countryman <stefan.countryman@gmail.com>
2018-08-27 09:39:40 -0400

    add lazy client login + cookies

--- igsync.py
+++ igsync.py
@@ -9,9 +9,18 @@ download the associated media.
 import os
 import sqlite3
 import json
+import logging
+import pickle
+from netrc import netrc
 from collections import namedtuple
+from http.cookiejar import CookieJar
 from textwrap import dedent
+from instagram_private_api import Client
+from instagram_private_api.errors import ClientConnectionError
 
+LOCAL_STORAGE = os.path.join(os.path.expanduser("~"), ".local", "share",
+                             "igsync")
+COOKIE_JAR = os.path.join(LOCAL_STORAGE, "cookie.jar")
 DEFAULT_COLLECTION_DIR = os.path.expanduser(
     os.path.join("~", "Pictures", "InstagramCollections")
 )
@@ -27,14 +36,14 @@ def dedent_sql(command):
 def get_media_links(media):
     """Extract the highest-quality media URLs (and media dimensions) from the
     ``media`` item in the JSON object returned by the Instagram API.
-    
+
     Arguments
     =========
     media : `string`, `dict`
         The only object contained in the ``post`` object returned by
         Instagram's posts API. can either be a JSON string or a dictionary (as
         parsed by ``json.loads``).
-    
+
     Returns
     =======
     links : list
@@ -59,14 +68,90 @@ def get_media_links(media):
 
 class InstagramDb(object):
     """A representation of the database where Instagram posts, users, and
-    collections are stored."""
+    collections are stored as well as an interface to the Instagram private
+    API."""
 
-    def __init__(self, path=DEFAULT_DB_PATH):
-        """``path`` is the path to the SQLite database that should be used.
+    def __init__(self, path=DEFAULT_DB_PATH, username=None, password=None,
+                 netrc_path=os.path.expanduser("~/.netrc")):
         """
+        Arguments
+        =========
+        path : `string`
+            the path to the SQLite database that should be used.
+        username : `string`, optional
+            instagram account username. If not provided, will be read from
+            ``netrc`` argument. If not available in a netrc file, user will be
+            prompted (if possible) for the username. If no username can be
+            found through these means, no connection will be made to the
+            Instagram API.
+        password : `string`, optional
+            instagram account password. If not provided, will be read from
+            ``netrc`` argument. If not available in a netrc file, user will be
+            prompted (if possible) for the password. If no password can be
+            found through these means, no connection will be made to the
+            Instagram API.
+        netrc_path : `string`, optional
+            path to the .netrc file from which the Instagram username and
+            password will be parsed (if not explicitly provided through init
+            arguments).
+        """
+        self.username = None  # will get overwritten when/if we log in
         self.path = os.path.realpath(path)
         self.connection = sqlite3.connect(self.path)
         self.cursor = self.connection.cursor()
+        self.netrc_path = netrc_path
+        # if username and password were explicitly provided, initialize a
+        # connection to instagram.com immediately. otherwise, this connection
+        # will be generated as needed.
+        if None not in (username, password):
+            self.login(username, password)
+        elif {username, password} != {None}:
+            raise ValueError("must provide both username and password or "
+                             "neither.")
+
+    def login(self, username, password):
+        """Try to form a new connection to Instagram using the given username
+        and password."""
+        # try to load cookies from cookie jar
+        client_kwargs = dict()
+        if os.path.isfile(COOKIE_JAR):
+            cookies = CookieJar()
+            with open(COOKIE_JAR, 'rb') as cookiejar:
+                cookie_string = cookiejar.read()
+            # make sure username in cookies is as expected; otherwise,
+            # don't use cookies
+            try:
+                cookies._cookies = pickle.loads(cookie_string)
+                cookie_user = [c.value for c in cookies
+                               if c.name == 'ds_user']
+                if cookie_user == username:
+                    client_kwargs['cookie'] = cookie_string
+            except:
+                logging.warn("Could not parse cookie file.")
+        self.client = Client(username, password, **client_kwargs)
+        self.username = username
+
+    @property
+    def client(self):
+        if not hasattr(self, "_client"):
+            # get user info from .netrc
+            try:
+                creds = netrc(self.netrc_path).authenticators("instagram.com")
+                self.login(creds[0], creds[2])
+            except:
+                raise ValueError("Can't find usable .netrc authentication "
+                                 "info in given .netrc: " + self.netrc_path)
+        return self._client
+
+    @client.setter
+    def client(self, value):
+        """Set the value of the client and cache the authentication cookies in
+        ~/.local/share/igsync/cookie.jar for later use."""
+        if not os.path.isdir(LOCAL_STORAGE):
+            os.makedirs(LOCAL_STORAGE)
+        self._client = value
+        with open(COOKIE_JAR, 'wb') as cookiejar:
+            cookiejar.write(self.client.cookie_jar.dump())
 
     TABLE_DEFINITIONS = namedtuple(
         'namespace',
@@ -170,7 +255,7 @@ class InstagramDb(object):
             self.connection.commit()
         return self
 
-    def save_collection(self, collection_pk, name="", overwrite=True,
+    def save_collection(self, collection_pk, name=None, overwrite=True,
                         commit=True):
         """Save an instagram collection to this database. ``collection_pk`` is
         the primary key of the collection as returned by Instagram's API and
@@ -179,10 +264,11 @@ class InstagramDb(object):
         Otherwise, only create the entry if the collection is not already
         saved. Returns ``self`` to allow for chained commands."""
         self.cursor.execute(
-            'INSERT OR {} INTO collections VALUES (?, "")'.format(
-                'REPLACE' if overwrite else 'IGNORE'
+            'INSERT OR {} INTO collections VALUES (?, {})'.format(
+                'REPLACE' if overwrite else 'IGNORE',
+                'NULL' if name is None else '?'
             ),
-            (collection_pk,)
+            (collection_pk,) if name is None else (collection_pk, name)
         )
         if commit:
             self.connection.commit()
@@ -210,9 +296,12 @@ class InstagramDb(object):
         return self
 
     def save_post(self, post, commit=True):
-        """Save an instagram post (as raw JSON returned from the API) to this
-        database. Returns ``self`` to allow for chained commands."""
-        media = json.loads(post)['media']
+        """Save an instagram post (as raw JSON returned from the API or as a
+        dict parsed from said JSON) to this database. Returns ``self`` to allow
+        for chained commands."""
+        if not isinstance(post, dict):
+            post = json.loads(post)
+        media = post['media']
         # make sure the user and collection are in their respective tables
         self.save_user(media['user'], overwrite=False, commit=False)
         for collection_pk in media['saved_collection_ids']:
@@ -248,3 +337,20 @@ class InstagramDb(object):
         if commit:
             self.connection.commit()
         return self
+
+    def get_anonymous_collections(self):
+        """Return a list of collection IDs whose names need to be set."""
+        self.cursor.execute("SELECT pk FROM collections WHERE name IS NULL")
+        return [res[0] for res in self.cursor.fetchall()]
+
+    def get_all_collections(self):
+        """Return a list of all locally saved collection IDs."""
+        self.cursor.execute("SELECT pk FROM collections")
+        return [res[0] for res in self.cursor.fetchall()]
+
+    def sync_collection_names(self, collection_pks):
+        """Get the latest names for the collections whose primary keys are
+        specified in ``collection_pks`` and save them to the local database."""
+        for pk in collection_pks:
+            name = self.client.collection_feed(pk)['collection_name']
+            self.save_collection(pk, name, overwrite=True)
